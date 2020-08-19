af7589ee775925f8eb7610c59ac0bd8353bab2e9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-18 23:21:06 -0600

    Clean up
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    # where(email: auth.info.email).first_or_initialize do |user|
-    #   user.name = auth.info.name
-    #   user.email = auth.info.email
-    # end
-
     where(user_id: auth.uid).first_or_create do |user|
       user.token = auth.credentials.token
       user.refresh_token = auth.credentials.refresh_token

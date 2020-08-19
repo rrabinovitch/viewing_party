9a688a9ec392c5c3c2d323773350fad01b959c12
Gaby Mendez <gmendez90@gmail.com>
2020-08-19 18:05:36 -0400

    Started testing code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "welcome#index", as: "root"
-  get "/auth/:provider/callback", to: "sessions#create"
+  get '/', to: 'welcome#index', as: 'root'
+  get '/auth/google_oauth2', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  # is the following route in case Google authentication fails?
+  # get 'auth/failure', to: redirect('/')
 end

4fd29c5e8ebc0e8330a5042854cdf2012a99c6b4
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-18 22:02:35 -0600

    Revert login route path
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index", as: "root"
-  get "/auth/google_oauth2/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create"
 end

9bd16b51af3a2742788e4b9af6c204a33baa5006
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-18 22:37:08 -0600

    Define User.from_omniauth and SessionsController.create
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,11 @@ class SessionsController < ApplicationController
   def create
     access_token = request.env["omniauth.auth"]
     user = User.from_omniauth(access_token)
-    log_in(user)
     user.token = access_token.credentials.token
     refresh_token = access_token.credentials.refresh_token
     user.refresh_token = refresh_token if refresh_token.present?
     user.save
-    binding.pry
-    # when root_path path helper works, switch this path out for that
-    redirect_to "/"
+    session[:user_id] = user.user_id
+    redirect_to dashboard_path
   end
 end

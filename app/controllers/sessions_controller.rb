610b06ce84a1a929aac4be4d33adb78d7eb41806
Gaby Mendez <gmendez90@gmail.com>
2020-08-19 19:12:53 -0400

    Removed redundant code from sessions#create, added route for dashboard#show as well as controller and view, changed redirect on sessions#create to dashboard#show.
    Co-authored-by: Ruthie Rabinovich <62635544+rrabinovitch@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,11 @@
 class SessionsController < ApplicationController
   def create
     access_token = request.env["omniauth.auth"]
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
     user = User.from_omniauth(access_token)
-    user.token = access_token.credentials.token
-    refresh_token = access_token.credentials.refresh_token
-    user.refresh_token = refresh_token if refresh_token.present?
-    user.save
     session[:user_id] = user.user_id
-    redirect_to "/"
+    # redirect_to "/"
     # redirect_to dashboard_path ### to be implemented for dashboard user story (issue #18)
+    redirect_to "/dashboard"
   end
 end

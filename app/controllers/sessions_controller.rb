fc826a737aab458c5c5f569f11d90930c8c4512d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-18 22:15:18 -0600

    Add refresh_token attribute to User; Define SessionsController#create
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
   def create
+    access_token = request.env["omniauth.auth"]
+    user = User.from_omniauth(access_token)
+    log_in(user)
+    user.token = access_token.credentials.token
+    refresh_token = access_token.credentials.refresh_token
+    user.refresh_token = refresh_token if refresh_token.present?
+    user.save
     binding.pry
+    # when root_path path helper works, switch this path out for that
+    redirect_to "/"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_032438) do
+ActiveRecord::Schema.define(version: 2020_08_19_041327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_032438) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "refresh_token"
   end
 
 end

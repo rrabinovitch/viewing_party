cf2b023acb108c78dd90821e02cf37b1cad0ed01
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-18 21:16:03 -0600

    Comment out unused route; fix reference to client_id
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index", as: "root"
   get "auth/:provider/callback", to: "sessions#googleAuth"
-  get "auth/failure", to: redirect("/")
+  # get "auth/failure", to: redirect("/") << this is from the medium article walkthrough - leaving it commented out until we work on a sadpath
 end

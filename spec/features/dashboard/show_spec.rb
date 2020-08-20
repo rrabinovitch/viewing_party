7c5e6e0b74557baff0304e5b16926e7b441207ba
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-19 22:31:43 -0600

    Fix dashboard spec: add login set up

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -1,15 +1,8 @@
-RSpec.describe 'user can log in' do
-  describe 'using google_oauth2', type: :feature do
+RSpec.describe 'dashboard show page' do
+  describe 'after logging in', type: :feature do
     before :each do
       visit root_path
-    end
-
-    it "user authenticates via Google, and is redirected to their dashboard where they see 'Welcome, <user_email>'" do
-      #
       click_on "Login"
-
-      expect(page).to have_content("Welcome, gmendez90@gmail.com")
-      # expect(page).to have_link("Logout")
     end
 
     it "the dashboard has a 'Discover Movies' button" do

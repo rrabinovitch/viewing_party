8c4b3fcaa5215e5515a5e02b5aeef380c8721b8e
Gaby Mendez <gmendez90@gmail.com>
2020-08-20 17:09:46 -0400

    Implement partial for movie search options section

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe 'As an authenticated user' do
     click_on "Login"
   end
 
-  it 'I should see a button to discover top-rated movies' do
-    #
+  it 'I should see a button to discover top-rated movies; when I click on it, I\'m taken to the Movies page' do
+    
     visit discover_path
-    expect(page).to have_button("Find Top Rated Movies")
+    click_on "Find Top Rated Movies"
+    expect(current_path).to eq(movies_path)
   end
 
   it 'I should also see a text field to enter a keyword(s) in the movie title and a "Find Movies" button' do
-    #
+
     visit discover_path
     expect(page).to have_selector("#movie_keywords")
     expect(page).to have_button("Find Movies")

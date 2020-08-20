cc4ac2256b7824efa462e72fec80990436bbb2fa
Gaby Mendez <gmendez90@gmail.com>
2020-08-20 16:42:18 -0400

    Add find movies button to discover#index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   get "/dashboard", to: "dashboard#show"
   get "/discover", to: "discover#index"
+  get "/movies", to: "movies#index"
 end

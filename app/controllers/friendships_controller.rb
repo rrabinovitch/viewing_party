a98e1a00f68977c49f41d3f8349da0ac9cb7a26a
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-20 15:27:09 -0600

    Add reciprocal friendship creation
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,9 @@ class FriendshipsController < ApplicationController
   def create
     friend_email = params[:email]
     @friend = User.find_by(username: friend_email)
-    unless @friend.nil?
+    if @friend
       Friendship.create(user_id: current_user.id, friend_id: @friend.id)
+      Friendship.create(user_id: @friend.id, friend_id: current_user.id)
       flash[:success] = "You have added #{@friend.username} as a friend."
     else
       flash[:error] = "User with email #{friend_email} does not exist."

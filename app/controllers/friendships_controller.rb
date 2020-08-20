9375fa8af8c6d293dbb861929053a44a6b8b0106
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-20 15:41:02 -0600

    Refactor with Friendship::create_reciprocal_friendship method to use in FriendshipsController#create action
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,8 +3,8 @@ class FriendshipsController < ApplicationController
     friend_email = params[:email]
     @friend = User.find_by(username: friend_email)
     if @friend
-      Friendship.create(user_id: current_user.id, friend_id: @friend.id)
-      Friendship.create(user_id: @friend.id, friend_id: current_user.id)
+      # Friendship.create(user_id: current_user.id, friend_id: @friend.id)
+      Friendship.create_reciprocal_friendship(current_user.id, @friend.id)
       flash[:success] = "You have added #{@friend.username} as a friend."
     else
       flash[:error] = "User with email #{friend_email} does not exist."
@@ -12,3 +12,22 @@ class FriendshipsController < ApplicationController
     redirect_to dashboard_path
   end
 end
+
+
+
+# def create
+#   #is this bulk addition or individual addition?
+#   if params.include?(:friend_id) #individual e.g. "Add friend" link
+#     @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, params[:friend_id])
+#   else
+#     params[:user][:friend_ids].each do |f_id|
+#       @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, f_id)
+#     end
+#   end
+#   redirect_to users_path
+# end
+#
+# def destroy
+#   Friendship.destroy_reciprocal_for_ids(current_user_id, params[:friend_id])
+#   redirect_to(request.referer)
+# end

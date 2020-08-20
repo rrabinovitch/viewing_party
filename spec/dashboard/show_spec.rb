ccb9cdf4716e98ca84ae143f2555e3e15e14c46f
Gaby Mendez <gmendez90@gmail.com>
2020-08-19 20:05:38 -0400

    Edit dashboard#show spec

--- spec/dashboard/show_spec.rb
+++ spec/dashboard/show_spec.rb
@@ -1,13 +1,12 @@
 RSpec.describe 'user can log in' do
   describe 'using google_oauth2', type: :feature do
 
-    it 'user signs in via Google, and is redirected to their dashboard' do
+    it 'user authenticates via Google, and is redirected to their dashboard where they see Welcome, user_email' do
       #
       visit root_path
       click_on "Login"
       # need to implement current user or something so that we can show info.
-      expect(page).to have_content("gmendez90@gmail.com")
-      expect(page).to have_content("Dashboard")
+      expect(page).to have_content("Welcome, gmendez90@gmail.com")
       # expect(page).to have_link("Logout")
     end
   end

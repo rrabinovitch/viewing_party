9d331123f78ba5e84e5a078acddae2f1379612f7
Gaby Mendez <gmendez90@gmail.com>
2020-08-20 21:54:46 -0400

    Rubocop cleanup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,13 +4,13 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org', headers: { 'Accept': 'application/json' })
 
     page1 = conn.get('/3/movie/top_rated') do |req|
-      req.params['api_key'] = ENV["MOVIE_DB_API_KEY_V3"]
+      req.params['api_key'] = ENV['MOVIE_DB_API_KEY_V3']
       req.params['language'] = 'en-US'
       req.params['page'] = '1'
     end
 
     page2 = conn.get('/3/movie/top_rated') do |req|
-      req.params['api_key'] = ENV["MOVIE_DB_API_KEY_V3"]
+      req.params['api_key'] = ENV['MOVIE_DB_API_KEY_V3']
       req.params['language'] = 'en-US'
       req.params['page'] = '2'
     end
@@ -18,9 +18,7 @@ class MoviesController < ApplicationController
     json1 = JSON.parse(page1.body, symbolize_names: true)
     json2 = JSON.parse(page2.body, symbolize_names: true)
 
-    until json2[:results].count == 0
-      json1[:results] << json2[:results].shift
-    end
+    json1[:results] << json2[:results].shift until json2[:results].count.zero?
 
     @movies = json1[:results]
   end

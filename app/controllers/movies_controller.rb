67455efbe44c206bc3b5d364f8686a920efba339
Gaby Mendez <gmendez90@gmail.com>
2020-08-23 15:15:48 -0400

    Refactor api conn & parsing out of movies#index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,27 +2,12 @@ class MoviesController < ApplicationController
   before_action :require_login
 
   def index
-    # Refactor this disaster into a MovieDB Poro?
-    conn = Faraday.new(url: 'https://api.themoviedb.org', headers: { 'Accept': 'application/json' })
+    movie_db = MovieDBService.new
+    page1 = movie_db.top_rated(1)
+    page2 = movie_db.top_rated(2)
 
-    page1 = conn.get('/3/movie/top_rated') do |req|
-      req.params['api_key'] = ENV['MOVIE_DB_API_KEY_V3']
-      req.params['language'] = 'en-US'
-      req.params['page'] = '1'
-    end
+    @movies = page1[:results] << page2[:results].shift until page2[:results].count.zero?
 
-    page2 = conn.get('/3/movie/top_rated') do |req|
-      req.params['api_key'] = ENV['MOVIE_DB_API_KEY_V3']
-      req.params['language'] = 'en-US'
-      req.params['page'] = '2'
-    end
-
-    json1 = JSON.parse(page1.body, symbolize_names: true)
-    json2 = JSON.parse(page2.body, symbolize_names: true)
-
-    json1[:results] << json2[:results].shift until json2[:results].count.zero?
-
-    @movies = json1[:results]
     # @movies = json1[:results].map do |movie_data|
     #   Movie.new(movie_data)
     # end
--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -1,10 +1,11 @@
 class MovieDBService
   def top_rated(page_num)
-    conn.get('/3/movie/top_rated') do |req|
+    response = conn.get('/3/movie/top_rated') do |req|
       req.params['api_key'] = ENV['MOVIE_DB_API_KEY_V3']
       req.params['language'] = 'en-US'
       req.params['page'] = page_num.to_s
     end
+    parse(response)
   end
 
   private

44359c622dc0cc4a98cfef790452ee7e57ae6ef0
Gaby Mendez <gmendez90@gmail.com>
2020-08-23 20:39:06 -0400

    Add #genres to Movie class

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -3,15 +3,20 @@ class Movie
   def initialize(movie_data)
     @id = movie_data[:id]
     @title = movie_data[:title]
+    @raw_hash ||= MovieDBService.new.find_by(@id)
   end
 
   def summary
-    movie = MovieDBService.new.find_by(@id)
-    movie[:overview]
+    @raw_hash[:overview]
   end
 
   def vote_avg
-    movie = MovieDBService.new.find_by(@id)
-    movie[:vote_average]
+    @raw_hash[:vote_average]
+  end
+
+  def genres
+    @raw_hash[:genres].map do |genre_hash|
+      genre_hash[:name]
+    end
   end
 end

938e26f787543002e6bc12ff0cb43698f4c4bf66
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-24 09:42:21 -0600

    Pseudo code / outline conditional routing for movies#index action
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,16 @@ class MoviesController < ApplicationController
   before_action :require_login
 
   def index
-    binding.pry
+    # refactor to an unless statement?
+    if params[:movie_keywords].nil?
+      @movies = SearchResults.new.top_forty
+    else
+      # binding.pry
+      # find movie results based on params[:movie_keywords]
+    end
     # if query params include keywords, perform that search,
     # else, show top40 results (those are the two routes to
     # get to this index action)
-    @movies = SearchResults.new.top_forty
   end
 
   def show

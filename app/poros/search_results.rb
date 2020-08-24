99607102bd0325ea5ccad5b67b8e50ae38d03047
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-24 17:35:45 -0600

    Change #count usage across app to #size
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/poros/search_results.rb
+++ app/poros/search_results.rb
@@ -6,7 +6,7 @@ class SearchResults
   def top_forty
     page1 = @service.top_rated(1)
     page2 = @service.top_rated(2)
-    movies = page1[:results] << page2[:results].shift until page2[:results].count.zero?
+    movies = page1[:results] << page2[:results].shift until page2[:results].size.zero?
     movies.map do |movie_data|
       get_movie(movie_data[:id])
     end

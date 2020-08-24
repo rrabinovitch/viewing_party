6b839dddbb36134543eaf8c9191a6776dcb575b5
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-24 11:55:13 -0600

    Create 'top_forty_movies' cassette
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,16 +8,17 @@ RSpec.describe 'As an authenticated user' do
   end
 
   it 'When I visit the movies page, I should see the 40 results from my search, I should also see the "Find Top Rated Movies" button and the Find Movies form at the top of the page.' do
+    VCR.use_cassette('top_forty_movies') do
+      visit discover_path
+      click_on "Find Top Rated Movies"
+      expect(current_path).to eq(movies_path)
 
-    visit discover_path
-    click_on "Find Top Rated Movies"
-    expect(current_path).to eq(movies_path)
+      expect(page).to have_selector("#movie_keywords")
+      expect(page).to have_button("Find Movies")
 
-    expect(page).to have_selector("#movie_keywords")
-    expect(page).to have_button("Find Movies")
-
-    within(".top40") do
-      expect(page).to have_selector(".movie-title", count: 40)
+      within(".top40") do
+        expect(page).to have_selector(".movie-title", count: 40)
+      end
     end
   end
 

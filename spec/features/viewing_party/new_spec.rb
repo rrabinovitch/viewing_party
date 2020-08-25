c9df3732f54e03a6c14e8ac5e8be68bedf45fd4c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-25 15:00:21 -0600

    Set up test for contents of new viewing party form

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -3,21 +3,19 @@ RSpec.describe 'As an authenticated user' do
     user = User.new(user_id: '7777', username: 'gaby@gmail.com', token: '1234gaby', refresh_token: 'gaby1234')
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    # movie = Movie.new(id: 1, title: "Gaby and Ruthie Create an App", vote_avg: 9.9, summary: "So many fun times!", genres: ["Thriller"], runtime: 150)
-    # allow_any_instance_of(MoviesController).to receive(:)
   end
 
   describe 'visit movie details page' do
     it 'When I click the button to create a viewing party, I\'m redirected to a new viewing party form' do
       VCR.use_cassette('find_dickie_roberts_by_id') do
         visit viewing_parties_new_path({movie_id: 13778})
-        expect()
-
+        expect(page).to have_selector("#movie_title")
+        # test that movie title field is un-editable
+        expect(page).to have_selector("#party_duration")
+        expect(page).to have_selector("#date")
+        expect(page).to have_selector("#friends")
+        expect(page).to have_button("Create Party")
       end
-      # expect one movie's details here.
-      # specify classes in show page and check that css is there (ie not empty)
-      # district = find(".district").text
-      # expect(district).not_to be_empty
     end
   end
 end

900c8878dc7cdf3326bd652f35f5cf78e9e295c8
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-25 16:33:52 -0600

    Start setup for test for filling in viewing party form

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -2,4 +2,8 @@ class ViewingPartiesController < ApplicationController
   def new
     @movie = SearchResults.new.get_movie(params[:movie_id])
   end
+
+  def create
+    
+  end
 end
--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'As an authenticated user' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
   end
 
-  describe 'visit movie details page' do
+  describe 'New viewing party form' do
     it 'With friends: When I click the button to create a viewing party, I\'m redirected to a new viewing party form' do
       VCR.use_cassette('find_dickie_roberts_by_id') do
         friend1 = User.create(user_id: '9999', username: 'ruthie@gmail.com', token: '4321ruthie', refresh_token: 'ruthie4321')
@@ -38,6 +38,31 @@ RSpec.describe 'As an authenticated user' do
         expect(page).to have_content("No friends to invite to your viewing party.")
 
         expect(page).to have_button("Create Party")
+      end
+    end
+  end
+
+  describe 'Filling out new viewing party form' do
+    it 'With friends: When I click the button to create a viewing party, I\'m redirected to a new viewing party form' do
+      VCR.use_cassette('find_dickie_roberts_by_id') do
+        friend1 = User.create(user_id: '9999', username: 'ruthie@gmail.com', token: '4321ruthie', refresh_token: 'ruthie4321')
+        friend2 = User.create(user_id: '1111', username: 'noodle@gmail.com', token: '1234noodle', refresh_token: 'noodle4321')
+        @current_user.friends << friend1
+        @current_user.friends << friend2
+
+        visit viewing_parties_new_path({movie_id: 13778})
+
+        fill_in :duration, with: 200
+        # page.check(friend1.username)
+
+
+        # expect(page).to have_selector("#date")
+        # expect(page).to have_selector("#friend_ids")
+        # expect(page).to have_content(friend1.username)
+        # expect(page).to have_content(friend2.username)
+        #
+        #
+        # expect(page).to have_button("Create Party")
       end
     end
   end

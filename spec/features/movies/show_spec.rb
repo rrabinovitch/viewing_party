e9b12a2872afc506ed9d15b131b8137653dadab9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-24 18:44:28 -0600

    Add class attributes to movies show page and test contents

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -24,11 +24,41 @@ RSpec.describe 'As an authenticated user' do
         find('.movie-title', match: :first).click
         expect(current_path).to eq(movies_detail_path)
         expect(page).to have_button("New Viewing Party")
-        # expect one movie's details here.
+
+        expect(page).to have_css(".movie-title")
+        movie_title = find(".movie-title").text
+        expect(movie_title).not_to be_empty
+
+        expect(page).to have_css(".vote-average")
+        vote_avg = find(".vote-average").text
+        expect(vote_avg).not_to be_empty
+
+        expect(page).to have_css(".runtime")
+        runtime = find(".runtime").text
+        expect(runtime).not_to be_empty
+
+        expect(page).to have_css(".summary")
+        summary = find(".summary").text
+        expect(summary).not_to be_empty
+
+        expect(page).to have_css(".genres")
+        genres = find(".genres").text
+        expect(genres).not_to be_empty
+
+        expect(page).to have_css(".cast-member")
+
+        ### the first move in the top 40 list does not have any reviews
+        ### we can either test a different movie's show page or not test reviews
+        # within(first(".movie-review")) do
+        #   expect(page).to have_css(".review-author")
+        #   author = find(".review_author").text
+        #   expect(author).not_to be_empty
+        #
+        #   expect(page).to have_css(".review-content")
+        #   content = find(".review-content").text
+        #   expect(content).not_to be_empty
+        # end
       end
     end
   end
-
-
-
 end

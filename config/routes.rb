1d909a4ca8784aa392c6bb988e6974f8ee2594f0
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-25 16:19:18 -0600

    Create viewing parties post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index", as: "root"
+
   get "/auth/:provider/callback", to: "sessions#create"
+
   get "/dashboard", to: "dashboard#show"
+
   get "/discover", to: "discover#index"
+
   get "/movies", to: "movies#index"
   get "/movies/detail", to: "movies#show"
+
   post "/friendships", to: "friendships#create"
+
   get "/viewing_parties/new", to: "viewing_parties#new"
+  post "/viewing_parties", to: "viewing_parties#create"
 end

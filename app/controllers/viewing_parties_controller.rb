babba2aa5df1efbb251153b045cb61b3b9f12ee5
Gaby Mendez <gmendez90@gmail.com>
2020-08-26 00:43:50 -0400

    Write ViewingPartyController#create, edit new form to include movie_id in query params and account for all checked attendees

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -4,5 +4,18 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
+    # for some reason, new party and new UserParty aren't being saved.
+    party = current_user.parties.create(party_params)
+    params[:users][:id].each do |user_id|
+      UserParty.create(party_id: party.id, attendee_id: user_id, host_id: current_user.id)
+    end
+    # add to Google Cal
+    redirect_to "/dashboard"
+  end
+
+  private
+
+  def party_params
+    params.permit(:movie_id, :duration, :date)
   end
 end

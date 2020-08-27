8c62dd1ca2390c0151442916395fceb67fb43e74
Gaby Mendez <gmendez90@gmail.com>
2020-08-27 16:58:56 -0400

    Remove unecessary code in controller

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,7 +2,7 @@ class DiscoverController < ApplicationController
   before_action :require_login
 
   def index
-    if params[:movie_keywords] && params[:q].nil?
+    if params[:movie_keywords]
       @movies = SearchResults.new.keyword_results(params[:movie_keywords])
     elsif params[:q]
       @movies = SearchResults.new.top_forty

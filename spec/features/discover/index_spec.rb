50b5b4896894ff6d2cc4beee2b9d52b9305ebeb6
Gaby Mendez <gmendez90@gmail.com>
2020-08-26 21:51:59 -0400

    Update discover/index_spec to reflect changes in paths and js addition

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -5,12 +5,17 @@ RSpec.describe 'As an authenticated user' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
-  it 'I should see a button to discover top-rated movies; when I click on it, I\'m taken to the Movies page' do
+  it 'I should see a button to discover top-rated movies; when I click on it, the page reloads with the top-rated movies' do
 
-    VCR.use_cassette('top_forty_movies') do
+    VCR.use_cassette('top_forty_movies', :allow_playback_repeats => true) do
       visit discover_path
       click_on "Find Top Rated Movies"
-      expect(current_path).to eq(movies_path)
+      # expect(current_path).to eq(movies_path)
+      # after adding JS
+      expect(current_path).to eq(discover_path)
+      # within("#search-results") do
+      #   expect(page).to have_selector(".movie-title", count: 40)
+      # end
     end
   end
 
@@ -21,13 +26,31 @@ RSpec.describe 'As an authenticated user' do
     expect(page).to have_button("Find Movies")
   end
 
-  it 'When I submit a movie keyword search, I am taken to the movies page' do
+  it 'When I submit a movie keyword search, the page reloads with search results' do
 
-    VCR.use_cassette('dickie_roberts_search_results') do
+    VCR.use_cassette('dickie_roberts_search_results', :allow_playback_repeats => true) do
       visit discover_path
       fill_in :movie_keywords, with: "Dickie Roberts"
       click_on "Find Movies"
-      expect(current_path).to eq(movies_path)
+      # expect(current_path).to eq(movies_path)
+      # after adding JS
+      expect(current_path).to eq(discover_path)
+      # within('#search-results') do
+      #   expect(page).to have_selector(".movie-title", count: 1)
+      # end
+    end
+  end
+
+  it 'Keyword search results return a maximum of 40 movies' do
+    VCR.use_cassette('phoenix_search_results') do
+      visit discover_path
+      fill_in :movie_keywords, with: "Phoenix"
+      click_on "Find Movies"
+
+      expect(current_path).to eq(discover_path)
+      # within('#search-results') do
+      #   expect(page).to have_selector(".movie-title", count: 40)
+      # end
     end
   end
 end

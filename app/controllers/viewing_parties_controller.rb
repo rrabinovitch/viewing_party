8ee81e08732b9866167471b45dea1cc0ed2a4686
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-26 20:25:01 -0600

    Implement google cal event creation functionality
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -7,18 +7,33 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
-    binding.pry
-    # instead, create party separately (without user id)
-    # then create the join table entires with current_user, host: true
     party = Party.create(party_params)
-    friends = params[:users][:id]
+    friends = params[:users][:id] unless params[:users].nil?
     unless friends.nil?
       friends.each do |user_id|
         UserParty.create(party_id: party.id, attendee_id: user_id)
       end
     end
+
     UserParty.create(party_id: party.id, attendee_id: current_user.id, is_host: true)
-    create_google_calendar_event(party.movie)
+
+    calendar = Google::Apis::CalendarV3
+    client = calendar::CalendarService.new
+
+    client.authorization = google_secret.to_authorization
+
+    event = Google::Apis::CalendarV3::Event.new(
+      summary: "#{party.movie} Viewing Party",
+      start: Google::Apis::CalendarV3::EventDateTime.new(
+        date: params[:date]
+      ),
+      end: Google::Apis::CalendarV3::EventDateTime.new(
+        date: params[:date] # fix end time to reflect duration
+      )
+    )
+
+    result = client.insert_event('primary', event)
+
     redirect_to dashboard_path
   end
 
@@ -28,30 +43,18 @@ class ViewingPartiesController < ApplicationController
     params.permit(:movie_id, :duration, :date, :time)
   end
 
-  def create_google_calendar_event(movie_title) #add , start_datetime, end_datetime args
-    event = Google::Apis::CalendarV3::Event.new({
-      summary: "#{movie_title} Watch Party",
-      start: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: '2015-05-28T09:00:00-07:00',
-        time_zone: 'America/Los_Angeles'
-        ),
-      end: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: '2015-05-28T17:00:00-07:00',
-        time_zone: 'America/Los_Angeles'
-        )
-      })
-
-  end
-
-
-
-  # def google_secret
-  #   Google::APIClient::ClientSecrets.new(
-  #     { 'web' =>
-  #       { 'access_token' => @user.google_token,
-  #         'refresh_token' => @user.google_refresh_token,
-  #         'client_id' => Rails.application.secrets.google_client_id,
-  #         'client_secret' => Rails.application.secrets.google_client_secret } }
-  #   )
+  # def create_google_cal_event ## to refactor out of #create action
   # end
+
+  def google_secret
+    Google::APIClient::ClientSecrets.new(
+      { "web" =>
+        { "access_token" => current_user.token,
+          "refresh_token" => current_user.refresh_token,
+          "client_id" => ENV['GOOGLE_CLIENT_ID'],
+          "client_secret" => ENV['GOOGLE_CLIENT_SECRET']
+        }
+      }
+    )
+  end
 end

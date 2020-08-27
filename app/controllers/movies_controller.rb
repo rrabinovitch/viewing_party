a4911bd69611d0e8e39cc3d5c2a7825ff4fa29a3
Gaby Mendez <gmendez90@gmail.com>
2020-08-26 21:49:34 -0400

    Move code from MoviesC#index to DiscoverC#index to serve movie results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,6 @@ class MoviesController < ApplicationController
   before_action :require_login
 
   def index
-    keywords = params[:movie_keywords]
-    if keywords.nil?
-      @movies = SearchResults.new.top_forty
-    else
-      @movies = SearchResults.new.keyword_results(keywords)
-    end
   end
 
   def show

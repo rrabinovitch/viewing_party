1b94e0a48406d6d8c39fb960e819f1ab75dbde9e
Gaby Mendez <gmendez90@gmail.com>
2020-08-26 21:56:17 -0400

    Add comments to discover#index_spec

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -5,25 +5,35 @@ RSpec.describe 'As an authenticated user' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
-  it 'I should see a button to discover top-rated movies; when I click on it, the page reloads with the top-rated movies' do
+  it 'I should see a button to discover top-rated movies; when I click on it, the page reloads with top 40 rated movies; I should also see the "Find Top Rated Movies" button and the Find Movies form at the top of the page.' do
 
     VCR.use_cassette('top_forty_movies', :allow_playback_repeats => true) do
+
       visit discover_path
+
+      expect(page).to have_selector("#movie_keywords")
+      expect(page).to have_button("Find Movies")
+
       click_on "Find Top Rated Movies"
-      # expect(current_path).to eq(movies_path)
-      # after adding JS
       expect(current_path).to eq(discover_path)
+
+      # After JS, css selectors don't work
       # within("#search-results") do
       #   expect(page).to have_selector(".movie-title", count: 40)
       # end
     end
   end
 
-  it 'I should also see a text field to enter a keyword(s) in the movie title and a "Find Movies" button' do
+  it 'When I click on a movie title link, I should be redirected to the movie\'s details page' do
+    VCR.use_cassette('first_of_top_forty_movies') do
+      visit discover_path
+      click_on "Find Top Rated Movies"
+      expect(current_path).to eq(discover_path)
 
-    visit discover_path
-    expect(page).to have_selector("#movie_keywords")
-    expect(page).to have_button("Find Movies")
+      # After JS, css selectors don't work
+      # find('.movie-title', match: :first).click
+      # expect(current_path).to eq(movies_detail_path)
+    end
   end
 
   it 'When I submit a movie keyword search, the page reloads with search results' do
@@ -32,9 +42,9 @@ RSpec.describe 'As an authenticated user' do
       visit discover_path
       fill_in :movie_keywords, with: "Dickie Roberts"
       click_on "Find Movies"
-      # expect(current_path).to eq(movies_path)
-      # after adding JS
       expect(current_path).to eq(discover_path)
+
+      # After JS, css selectors don't work
       # within('#search-results') do
       #   expect(page).to have_selector(".movie-title", count: 1)
       # end
@@ -48,6 +58,8 @@ RSpec.describe 'As an authenticated user' do
       click_on "Find Movies"
 
       expect(current_path).to eq(discover_path)
+
+      # After JS, css selectors don't work
       # within('#search-results') do
       #   expect(page).to have_selector(".movie-title", count: 40)
       # end

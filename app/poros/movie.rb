66781ccea6803b4e3ec6a5f1f3699ddbb81dccd5
Gaby Mendez <gmendez90@gmail.com>
2020-08-27 17:10:48 -0400

    Refactor movie#genres code

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,17 +1,21 @@
 # Movie PORO - does not save in DB!
 class Movie
-  attr_reader :id, :title, :vote_avg, :summary, :genres, :runtime
+  attr_reader :id, :title, :vote_avg, :summary, :runtime
 
   def initialize(movie_data)
     @id = movie_data[:id]
     @title = movie_data[:title]
     @vote_avg = movie_data[:vote_average]
     @summary = movie_data[:overview]
-    @genres = movie_data[:genres].map do |genre_hash|
-      genre_hash[:name]
-    end
+    @raw_genres = movie_data[:genres]
     @runtime = movie_data[:runtime]
     @search_results ||= SearchResults.new
+  end
+
+  def genres
+    @raw_genres.map do |genre_hash|
+      genre_hash[:name]
+    end
   end
 
   def first_ten_cast_members

1173ca324c4e24cb1ba3e515e97ef580e2b1aba0
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-26 19:57:50 -0600

    Update omniauth scope
    Co-authored-by: Gaby Mendez <62727545+gabichuelas@users.noreply.github.com>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: "userinfo.email, calendar"}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    access_type: "offline",
+    prompt: "consent",
+    select_account: true,
+    skip_jwt: true,
+    scope: "userinfo.email, calendar"}
 end
